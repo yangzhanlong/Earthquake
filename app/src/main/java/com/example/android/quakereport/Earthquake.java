b61852322da1d4bad187a23418140ddf67b64ff8
Zhanlong Yang <673029686@qq.com>
2016-12-19 18:49:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,45 @@
+package com.example.android.quakereport;
+
+import java.lang.ref.SoftReference;
+
+/**
+ * Created by Administrator on 2016/12/12.
+ */
+public class Earthquake {
+    private double mMag;
+    private String mPlace;
+
+    /** Time of the earthquake */
+    private long mTime;
+
+    private String mUrl;
+    /**
+     * 构造一个新的 {@link Earthquake} 对象。
+     *
+     * @param mag 表示地震的震级（大小）
+     * @param place 表示地震的城市位置
+     * @param time 表示地震发生时以毫秒（根据 Epoch）计的时间
+     */
+    public Earthquake(double mag, String place, long time, String url) {
+        mMag = mag;
+        mPlace = place;
+        mTime = time;
+        mUrl = url;
+    }
+
+    public double getmMag() {
+        return mMag;
+    }
+
+    public String getmPlace() {
+        return mPlace;
+    }
+
+    public long getmTime() {
+        return mTime;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+}
